65ce08d041f983e17b66a57851af2086ab87b3d6
MosutLarisa <146415153+MosutLarisa@users.noreply.github.com>
2023-10-13 15:25:12 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿namespace Tabel_adevar_AND;
+using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("Bitwise AND Truth Table");
+        Console.WriteLine("");
+        Console.WriteLine("|  A  |  B  | A & B |");
+
+        for (int A = 0; A <= 1; A++)
+        {
+            for (int B = 0; B <= 1; B++)
+            {
+                int result = A & B;
+                Console.WriteLine($"|  {A}  |  {B}  |   {result}   |");
+            }
+        }
+
+        Console.WriteLine();
+    }
+}
